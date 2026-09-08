fd0b3b40a91b5e072b3e0193e038e582c722023c
Nitesh <niti2002sh@gmail.com>
2026-09-09 02:34:15 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+export const app = express();
+
+app.use(express.json());
+
+app.get("/health", (_req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Todo Redis API is healthy",
+    })
+})
